d82b61deca2c5f910840862735255229f2b17c3c
4hm3d <ja_mokeddem@esi.dz>
2024-05-24 10:25:35 +0200

    Final Code

--- dashboard/🌎_Home.py
+++ dashboard/🌎_Home.py
@@ -10,7 +10,7 @@ st.set_page_config(
     page_icon="🌎",
     layout="wide"
 )
-
+print(st.get_option('theme.backgroundColor'))
 
 df=pd.read_csv("dashboard/data/df_mergedFinal.csv")
 df.loc[df["Country"]=="Antigua and Barbuda", 'Continent']="north-america"
